c437146d7e2d16d316930a605d0677037cb6a8bb
azad kemerbendi <kemerbendiazad@gmail.com>
2020-02-06 10:18:38 +0400

    check age

---
+++ tests/User/UserTest.php
@@ -0,0 +1,30 @@
+<?php
+
+
+use PHPUnit\Framework\TestCase;
+use App\Models\User;
+//include 'src/Classes/Models/User.php';
+
+class UserTest extends TestCase
+{
+    private $user;
+
+    protected function setUp(): void
+    {
+        $this->user = new User();
+        $this->user->setAge(33);
+    }
+
+    protected function tearDown(): void
+    {
+
+    }
+
+    /**
+     *
+     */
+    public function testAge()
+    {
+        $this->assertEquals(33, $this->user->getAge());
+    }
+}
